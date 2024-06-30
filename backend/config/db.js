9077f0db8001d729ff398a686ac2573ae7819308
sarath <asarath.94@gmail.com>
2024-06-30 14:34:17 +0530

    second commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-export const  connectDB = async () =>{
-
-    await mongoose.connect('{ Add your mongodb URI Here }/food-del').then(()=>console.log("DB Connected"));
-   
-}
-
+export const connectDB = async () => {
+  await mongoose
+    .connect(
+      "mongodb+srv://asarath94:sarath1994@cluster0.n1rbe8z.mongodb.net/food-del"
+    )
+    .then(() => console.log("DB Connected"));
+};
 
 // add your mongoDB connection string above.
 // Do not use '@' symbol in your databse user's password else it will show an error.
