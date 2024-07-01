f8149210175a7f25436afa2dd42a2ead3fbf02b2
asarath94 <64015362+asarath94@users.noreply.github.com>
2024-07-01 14:42:56 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,9 +3,15 @@ import './FoodDisplay.css'
 import FoodItem from '../FoodItem/FoodItem'
 import { StoreContext } from '../../Context/StoreContext'
 
+const backendUrl = 'https://food-delivery-backend-sxi3.onrender.com';
+
 const FoodDisplay = ({category}) => {
 
   const {food_list} = useContext(StoreContext);
+  
+  const getImageUrl = (imageName) => {
+    return `${backendUrl}/uploads/${imageName}`;
+  };
 
   return (
     <div className='food-display' id='food-display'>
@@ -13,7 +19,7 @@ const FoodDisplay = ({category}) => {
       <div className='food-display-list'>
         {food_list.map((item)=>{
           if (category==="All" || category===item.category) {
-            return <FoodItem key={item._id} image={item.image} name={item.name} desc={item.description} price={item.price} id={item._id}/>
+            return <FoodItem key={item._id} image={getImageUrl(item.image)} name={item.name} desc={item.description} price={item.price} id={item._id}/>
           }
         })}
       </div>
